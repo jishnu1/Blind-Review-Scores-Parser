8ec65e5b6739a2edf1038785c8348f233639eaac
Jishnu <jishnupatel1@gmail.com>
2025-05-09 18:31:03 -0500

    several changes

--- scraper.py
+++ scraper.py
@@ -1,18 +1,56 @@
 import csv
+import json
 import requests
 import time
 from bs4 import BeautifulSoup
 
-# MODIFY THESE
-INPUT_FILE_PATH = "input/input_5.txt"
-OUTPUT_FILE_PATH = "output/output_5.csv"
-TIME_DELAY = 1 # seconds
+# CONFIGURATION (MODIFY THESE)
+
+# Paths to input, output, and database files
+INPUT_FILE_PATH    = "input/input_5.txt"
+OUTPUT_FILE_PATH   = "output/output_5.csv"
+DATABASE_FILE_PATH = "database/database.json"
+
+# Set fields you want in the output file to True, and those you don't want to False
+# The fields in the output file will be in the same order as they are in this dictionary (MAYBE)
+HEADERS = {
+    # general
+    "company_name": True,
+    "url":          True,
+    # overview
+    "website":   False,
+    "industry":  True,
+    "locations": True,
+    "founded":   False,
+    "size":      True,
+    "salary":    False,
+    # reviews
+    "overall":               True,
+    "career_growth":         True,
+    "work_life_balance":     True,
+    "compensation_benefits": True,
+    "company_culture":       True,
+    "management":            True,
+    # compensation"
+    "median_total_compensation": True,
+    "25th_percentile": False,
+    "70th_percentile": False,
+    "90th_percentile": False,
+    # other
+    "last_updated":    True
+}
+
+# Set the time delay (seconds) between requests to avoid overwhelming the server
+TIME_DELAY = 1
 
 # CONSTANTS
+
 BLIND_URL = "https://www.teamblind.com/company/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
+
+# HELPER FUNCTIONS
 
 def build_url(company_name):
     formatted_name = format_company_name(company_name)
@@ -21,25 +59,46 @@ def build_url(company_name):
 def format_company_name(company_name):
     return company_name.replace(" ", "-").lower()
 
+def read_database_file():
+    try:
+        with open(DATABASE_FILE_PATH, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+def write_database_file(database):
+    with open(DATABASE_FILE_PATH, "w") as file:
+        json.dump(database, file, indent=4)
+
 def read_input_file():
     with open(INPUT_FILE_PATH, "r") as file:
         company_names = file.readlines()
     return [name.strip() for name in company_names]
 
-def parse_review_scores(company_names):
-    review_data = []
-    for company_name in company_names:
-        url = build_url(company_name)
-        response = requests.get(url, headers=HEADERS)
-        soup = BeautifulSoup(response.content, "html.parser")
-        if response.status_code != 200:
-            print(f"Failed to retrieve data for {company_name}. Status code: {response.status_code}")
-            continue
+def get_company_data_from_database(database, company_name):
+    if company_name in database:
+        return database[company_name]
+    else:
+        return None
+
+def get_company_data_from_blind(database, company_name):
+    url = build_url(company_name)
+    response = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(response.content, "html.parser")
+    if response.status_code != 200:
+        print(f"Failed to retrieve data for {company_name}. Status code: {response.status_code}")
+    else:
         try:
+            # general
             company_name_tag = soup.find("div", class_="text-center text-2xl font-bold lg:text-left")
             company_name = company_name_tag.text.strip() if company_name_tag else None
             link_tag = soup.find("link", rel="canonical")
             url = link_tag["href"] if link_tag and "href" in link_tag.attrs else None
+
+            # overview
+            # TODO
+
+            # reviews
             overall_score = soup.find("div", class_="flex items-start gap-2 border-b border-gray-300 pb-1 pr-4 lg:flex-col lg:border-b-0 lg:border-r").find("div", class_="text-xl font-semibold").text.strip()
             subscores_div = soup.find("div", class_="grid grid-flow-row grid-cols-1 gap-x-10 gap-y-4 lg:ml-9 lg:grid-cols-2").find_all("div", class_="font-semibold")
             career_growth_score = subscores_div[0].text.strip()
@@ -47,46 +106,77 @@ def parse_review_scores(company_names):
             compensation_benefits_score = subscores_div[2].text.strip()
             company_culture_score = subscores_div[3].text.strip()
             management_score = subscores_div[4].text.strip()
+
+            # compensation
+            # TODO
+
             company_data = {
+                # general
                 "company_name": company_name,
+                "url": url,
+                # overview
+                "website": None,
+                "industry": None,
+                "locations": None,
+                "founded": None,
+                "size": None,
+                "salary": None,
+                # reviews
                 "overall": overall_score,
                 "career_growth": career_growth_score,
                 "work_life_balance": work_life_balance_score,
                 "compensation_benefits": compensation_benefits_score,
                 "company_culture": company_culture_score,
                 "management": management_score,
-                "url": url
+                # compensation
+                "median_total_compensation": None,
+                "25th_percentile": None,
+                "70th_percentile": None,
+                "90th_percentile": None,
+                # other
+                "last_updated": None
             }
-            review_data.append((company_name, company_data))
-        except AttributeError:
-            print(f"Review scores not found for {company_name}.")
-        time.sleep(TIME_DELAY)
+            database[company_name] = company_data
+        except AttributeError as e:
+            print(f"Error parsing data for {company_name}: {e}")
+            return None
+    return company_data
 
-    return review_data
-
-def write_output_file(review_data):
+def process_data(company_names):
     with open(OUTPUT_FILE_PATH, "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["Company Name", "Overall", "Career Growth", "Work-Life Balance", "Compensation & Benefits", "Company Culture", "Management", "URL"])
-        for company_name, company_data in review_data:
-            writer.writerow([
-                company_data["company_name"],
-                company_data["overall"],
-                company_data["career_growth"],
-                company_data["work_life_balance"],
-                company_data["compensation_benefits"],
-                company_data["company_culture"],
-                company_data["management"],
-                company_data["url"]
-            ])
+        header_row = []
+        for key, value in HEADERS.items():
+            if value:
+                header_row.append(key)
+        writer.writerow(header_row)
+        for i, company_name in enumerate(company_names):
+            print(f"Processing {i + 1}/{len(company_names)}: {company_name}")
+            company_data = get_company_data_from_database(database, company_name)
+            if company_data:
+                print("Got data from database")
+            else:
+                company_data = get_company_data_from_blind(database, company_name)
+                if company_data:
+                    print("Got data from Blind")
+                time.sleep(TIME_DELAY)
+            if company_data:
+                data_row = []
+                for key, value in HEADERS.items():
+                    if value:
+                        if key in company_data:
+                            data_row.append(company_data[key])
+                        else:
+                            data_row.append("")
+                writer.writerow(data_row)
+            else:
+                writer.writerow(company_name)
 
-def print_review_data(review_data):
-    for company_name, review_score in review_data:
-        print(f"{company_name}:\t{review_score}")
 
-# MAIN
+# MAIN FUNCTION
+
 if __name__ == "__main__":
     company_names = read_input_file()
-    review_data = parse_review_scores(company_names)
-    write_output_file(review_data)
-    print_review_data(review_data)
+    database = read_database_file()
+    process_data(company_names)
+    write_database_file(database)
